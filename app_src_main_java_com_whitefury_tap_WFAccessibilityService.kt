699576e44edd5500c5b37ed14de2cce379ca2a68
ajandhaprawin-netizen <ajandhaprawin@gmail.com>
2026-01-01 14:52:40 +0530

    Implement WFAccessibilityService for tap gestures

---
+++ app_src_main_java_com_whitefury_tap_WFAccessibilityService.kt
@@ -0,0 +1,21 @@
+package com.whitefury.tap
+
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
+import android.graphics.Path
+import android.view.accessibility.AccessibilityEvent
+
+class WFAccessibilityService : AccessibilityService() {
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
+    override fun onInterrupt() {}
+
+    fun tap(x: Int, y: Int) {
+        val path = Path()
+        path.moveTo(x.toFloat(), y.toFloat())
+        val gesture = GestureDescription.Builder()
+            .addStroke(GestureDescription.StrokeDescription(path, 0, 100))
+            .build()
+        dispatchGesture(gesture, null, null)
+    }
+}
